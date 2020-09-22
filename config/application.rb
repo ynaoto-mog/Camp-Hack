1c2ff517212cc5dd2def5e6037815b632116a566
apple-yagi <applesryu@gmail.com>
2020-09-22 22:18:03 +0900

    corsを設定

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,15 @@ module CampHack
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # CORS settings
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "https://camp-hackathon.netlify.app/",
+        resource "*",
+        headers: :any,
+        methods: [:get, :post, :options, :head]
+      end
+    end
   end
 end
