8976081d172935b45e6938e6e31d8c026aae0de8
ynaoto-mog <ynaoto.engi@gmail.com>
2020-09-23 23:39:11 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :likes, only: [:index, :create]
     resources :comments, only: [:index, :create, :destroy]
   end
-  mount ActionCable.server => '/cable'
+  # mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
