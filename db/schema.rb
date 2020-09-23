11f9f802843e7b573d53261ae1acd7c5c47116e8
ynaoto-mog <ynaoto.engi@gmail.com>
2020-09-24 02:33:35 +0900

    change data in user

--- db/schema.rb
+++ db/schema.rb
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(version: 2020_09_23_160629) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "nick_name"
-    t.integer "login_id"
+    t.string "nickname"
+    t.string "username"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
