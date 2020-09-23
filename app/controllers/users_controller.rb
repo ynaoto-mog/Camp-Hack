fc396731fb806895c32711be722e1fa8df708c15
ynaoto-mog <ynaoto.engi@gmail.com>
2020-09-24 00:44:56 +0900

    update users info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,20 @@
 class UsersController < ApplicationController
+    before_action :set_users, only: [sign_in]
     def sign_up
         user = User.new(user_params)
+        render json: user
     end
 
     def sign_in
-
+        render json: @user
     end
 
     private
     def user_params
         params.permit(:nick_name, :login_id, :password)
+    end
+
+    def set_users
+        @user = User.find_by(login_id: params[:id], password: params[:password])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     validates :nick_name, presence: true
     validates :login_id, presence: true, uniqueness: true, length: { minimum: 6 }
+    validates :password, presence: true, uniqueness: true, length: { minimum: 6 }
 end
