9eaff53131b6c2c0d92eb08e18d7d1bbd645e596
ynaoto-mog <ynaoto.engi@gmail.com>
2020-09-24 13:32:06 +0900

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Insect.create(:name => "カブトムシ", :description => "perfect", :latitude => 36.2, :longitude => 138.3, :image => "https://firebasestorage.googleapis.com/v0/b/camp-h...")
-Insect.create(:name => "とり", :description => "エモい", :latitude => 33.6, :longitude => 130.4, :image => "https://firebasestorage.googleapis.com/v0/b/camp-h...")
-Insect.create(:name => "クワガタ", :description => "でかい", :latitude => 38, :longitude => 140, :image => "https://firebasestorage.googleapis.com/v0/b/camp-h...")
+Insect.create({:name => "カブトムシ", :description => "perfect", :latitude => 36.2, :longitude => 138.3, :image => "https://firebasestorage.googleapis.com/v0/b/camp-h..."})
+Insect.create({:name => "とり", :description => "エモい", :latitude => 33.6, :longitude => 130.4, :image => "https://firebasestorage.googleapis.com/v0/b/camp-h..."})
+Insect.create({:name => "クワガタ", :description => "でかい", :latitude => 38, :longitude => 140, :image => "https://firebasestorage.googleapis.com/v0/b/camp-h..."})
