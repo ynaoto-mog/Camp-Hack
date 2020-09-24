4fb384ebc779afff46223288660feec462ffa5e7
ynaoto-mog <ynaoto.engi@gmail.com>
2020-09-24 12:23:49 +0900

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Insect.create(name: "カブトムシ", description: "perfect", latitude: 36.2, longtitude: 138.3, image:"https://firebasestorage.googleapis.com/v0/b/camp-h...")
